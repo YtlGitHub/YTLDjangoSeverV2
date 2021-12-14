41b62fe530adcc63d287569d07df13e4a891edef
杨天龙云服务器 <1766842398@qq.com>
2021-12-14 09:48:39 +0800

    ytl删除n=1

--- myobject/myweb/views/ytl.py
+++ myobject/myweb/views/ytl.py
@@ -2,13 +2,13 @@
 from django.shortcuts import render
 
 
-def ytl_url(request, n=1):
+def ytl_url(request):
     '''我的常用网址'''
     context = {"myweb_ytl_ytl_url_menu_open": "menu-open", "myweb_ytl_ytl_url_active": "active"}
     return render(request, "myweb/ytl/YTLURL.html", context)
 
 
-def phone_info(request, n=1):
+def phone_info(request):
     '''adb命令查看手机信息'''
     context = {"myweb_ytl_phone_info_menu_open": "menu-open", "myweb_ytl_phone_info_active": "active"}
     return render(request, "myweb/ytl/phoneInfo.html", context)
